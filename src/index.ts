e858c738077831db4f0ca5d4de4727734469bf86
Marc Skwarczynski <marc.skwarczynski@revature.net>
2021-06-25 11:52:46 -0700

    Added endpoints on router.

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,49 @@
 import express, { Request, Response } from 'express';
 
-const port = 3000;
+const PORT = 3000;
+const APP = express();
 
-const app = express();
+/** Set up Express Router */
+APP.use(express.json());
+APP.use(express.urlencoded({ extended: true }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('hello peop');
+/** Set up Root Endpoint (not used at the moment) */
+APP.get('/', (req: Request, res: Response) => {
+  res.send('This endpoint is not in use by our API.');
 });
 
-app.listen(port, () => {
-  console.log('Hello');
+/** Set up Users Endpoint */
+APP.get('/user/:username', (req: Request, res: Response) => {
+  res.send('This is where I would send you user data...If I had any!');
+});
+APP.post('/user/:username', (req: Request, res: Response) => {
+  res.send('Your request to add a new user has been received and ignored.');
+});
+APP.put('/user/:username', (req: Request, res: Response) => {
+  res.send('Your request to update a user has been received and ignored.');
+});
+APP.delete('/user/:username', (req: Request, res: Response) => {
+  res.send('Your request to delete a user has been received and ignored.');
+});
+
+/** Set up Chirps Endpoint */
+APP.get('/chirp/all', (req: Request, res: Response) => {
+  res.send('This is where I would send you all my chirps...If I had any!');
+});
+APP.get('/chirp/:username', (req: Request, res: Response) => {
+  res.send('This is where I would send you all the chirps from this user...If I had any!');
+});
+APP.post('/chirp', (req: Request, res: Response) => {
+  res.send('Your request to add a new chirp has been received and ignored.');
+});
+APP.put('/chirp/:timestamp', (req: Request, res: Response) => {
+  res.send('Your request to edit a chirp has been received and ignored.');
+});
+APP.delete('/chirp/:timestamp', (req: Request, res: Response) => {
+  res.send('Your request to delete a chirp has been received and ignored.');
+});
+
+/** Begin Listening for Requests */
+APP.listen(PORT, () => {
+  console.log('Hi! We are up and listening on port: ', PORT);
 });
