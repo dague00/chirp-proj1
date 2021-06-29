ed0c444d5d54bf34fe0b831dba508e333b7beb2a
Marc Skwarczynski <marc.skwarczynski@revature.net>
2021-06-29 13:24:38 -0700

    Updated chirp endpoints and fixed POST /user endpoint.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response } from 'express';
-import StatusCodes from 'http-status-codes';
 
 const PORT = 3000;
 const APP = express();
@@ -13,27 +12,18 @@ APP.use(express.urlencoded({ extended: true }));
 /** Set up Users Endpoint */
 APP.get('/user/all', getAllUsers);
 APP.get('/user/:username', getUser);
-APP.post('/user/:username', createOneUser);
+APP.post('/user', createOneUser);
 APP.put('/user/:username/bio', editUserBio);
 APP.put('/user/:username/password', editUserPassword);
 APP.delete('/user/:username', deleteUser);
 
 /** Set up Chirps Endpoint */
-APP.get('/chirp/all', (req: Request, res: Response) => {
-  res.send('This is where I would send you all my chirps...If I had any!');
-});
-APP.get('/chirp/:username', (req: Request, res: Response) => {
-  res.send('This is where I would send you all the chirps from this user...If I had any!');
-});
-APP.post('/chirp', (req: Request, res: Response) => {
-  res.send('Your request to add a new chirp has been received and ignored.');
-});
-APP.put('/chirp/:timestamp', (req: Request, res: Response) => {
-  res.send('Your request to edit a chirp has been received and ignored.');
-});
-APP.delete('/chirp/:timestamp', (req: Request, res: Response) => {
-  res.send('Your request to delete a chirp has been received and ignored.');
-});
+APP.get('/chirp/all', getChirps);
+APP.get('/chirp/:username', getUserChirps);
+APP.get('/chirp/:timestamp', getAChirp);
+APP.post('/chirp', postOneChirp);
+APP.put('/chirp/:timestamp', updateChirp);
+APP.delete('/chirp/:timestamp', deleteOneChirp);
 
 /** Begin Listening for Requests */
 APP.listen(PORT, () => {
